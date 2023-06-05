29e642cd820999de3062c60c316e72028ff56cc0
Piedro Rockembach Nunes <piedrorn@gmail.com>
2023-06-05 20:00:35 -0300

    linter fix

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,8 +1,8 @@
-import Dwarf from "./Dwarf";
-import Elf from "./Elf";
-import Race from "./Race";
-import Halfling from "./Halfling";
-import Orc from "./Orc";
+import Dwarf from './Dwarf';
+import Elf from './Elf';
+import Race from './Race';
+import Halfling from './Halfling';
+import Orc from './Orc';
 
 export {
   Dwarf,
