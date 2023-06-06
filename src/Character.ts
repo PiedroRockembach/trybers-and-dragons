11c7dcd44874ec166a1a6771b560ded587e6eee9
Piedro Rockembach Nunes <piedrorn@gmail.com>
2023-06-05 23:26:50 -0300

    cria interface simpleFighter

--- src/Character.ts
+++ src/Character.ts
@@ -21,7 +21,7 @@ export default class Character implements Fighter {
     this._race = new Elf(name, this._dexterity);
     this._archetype = new Mage(name);
     this._maxLifePoints = this._race.maxLifePoints / 2;
-    this._lifePoints = this._race.maxLifePoints;
+    this._lifePoints = this._maxLifePoints;
     this._strength = rng(10);
     this._defense = rng(10);
     this._energy = { amount: rng(10), type_: this._archetype.energyType };
@@ -49,13 +49,13 @@ export default class Character implements Fighter {
 
   levelUp(): void {
     this._maxLifePoints += getRandomInt(1, 10);
-    if (this._maxLifePoints > this._race.maxLifePoints) {
-      this._maxLifePoints = this._race.maxLifePoints;
-    }
     this._strength += getRandomInt(1, 10);
     this._dexterity += getRandomInt(1, 10);
     this._defense += getRandomInt(1, 10);
     this._energy.amount = 10;
+    if (this._maxLifePoints > this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
     this._lifePoints = this._maxLifePoints;
   }
 
